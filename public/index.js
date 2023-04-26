4ad5b37847809e3d72734af86d4644e8ec807ba3
SiaESin <siabhon.sindab@gmail.com>
2023-04-26 15:02:21 -0700

    I've gotten nowhere

--- public/index.js
+++ public/index.js
@@ -4,6 +4,14 @@ async function main() {
     const highestPriceChartCanvas = document.querySelector('#highest-price-chart');
     const averagePriceChartCanvas = document.querySelector('#average-price-chart');
 
-}
+    const response = await fetch('https://api.twelvedata.com/time_series?symbol=GME,MSFT,DIS,BTNX&interval=1min&apikey=fe1dcefef44145ae82198e86b2ab52bc')
+    const result = await response.json() 
+    const {GME, MSFT, DIS, BTNX} = result
 
+    const stocks = [GME, MSFT, DIS, BNTX];
+
+
+console.log(result);
+}
+//fe1dcefef44145ae82198e86b2ab52bc don't forget to remove
 main()
